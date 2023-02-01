966da40b37013d5126cc8b960d8bd396bf001d93
Hyune-c <choi8608@gmail.com>
2023-02-01 13:45:12 +0900

    init

---
+++ src/main/kotlin/com/example/openfeignspringtutorial/OpenfeignSpringTutorialApplication.kt
@@ -0,0 +1,11 @@
+package com.example.openfeignspringtutorial
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OpenfeignSpringTutorialApplication
+
+fun main(args: Array<String>) {
+    runApplication<OpenfeignSpringTutorialApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/openfeignspringtutorial/OpenfeignSpringTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.openfeignspringtutorial
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OpenfeignSpringTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
