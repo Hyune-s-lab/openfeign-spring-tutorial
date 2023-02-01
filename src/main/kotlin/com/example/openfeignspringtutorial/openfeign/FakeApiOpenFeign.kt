5119f518845a2fb85db8948934234a4f46b2dd66
Hyune-c <choi8608@gmail.com>
2023-02-01 18:14:55 +0900

    🔧 feign-url 환경 변수로 분리

--- src/main/kotlin/com/example/openfeignspringtutorial/openfeign/FakeApiOpenFeign.kt
+++ src/main/kotlin/com/example/openfeignspringtutorial/openfeign/FakeApiOpenFeign.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import java.util.*
 
-@FeignClient(name = "fake-api", url = "https://jsonplaceholder.typicode.com")
+@FeignClient(name = "fake-api", url = "\${application.feign-url.fake-api}")
 interface FakeApiOpenFeign {
     @GetMapping("/posts/{postId}")
     fun getPost(@PathVariable postId: Long): PostResponse
