18af141f1aa365ee54efad1d2c1312ca5726459f
Hyune-c <choi8608@gmail.com>
2023-02-02 14:16:54 +0900

    ✅ getPost stub - success assertion 까지 구현

--- src/test/kotlin/com/example/openfeignspringtutorial/openfeign/BooksClientIntegrationTest.kt
+++ src/test/kotlin/com/example/openfeignspringtutorial/openfeign/BooksClientIntegrationTest.kt
@@ -1,22 +1,40 @@
 package com.example.openfeignspringtutorial.openfeign
 
+import com.example.openfeignspringtutorial.openfeign.response.PostResponse
 import com.example.openfeignspringtutorial.testconfig.FeignSuccessStub
 import com.example.openfeignspringtutorial.testconfig.WireMockConfig
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.google.gson.Gson
+import com.google.gson.stream.JsonReader
 import io.kotest.core.spec.Spec
-import io.kotest.core.spec.style.FunSpec
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
+import java.io.FileReader
 
 @ActiveProfiles("test")
 @SpringBootTest(classes = [WireMockConfig::class])
 internal class BooksClientIntegrationTest(
     private val wireMockServer: WireMockServer,
     private val fakeApiOpenFeign: FakeApiOpenFeign,
-) : FunSpec({
-    test("success stub - getPosts") {
-        FeignSuccessStub.getPosts(wireMockServer)
-        fakeApiOpenFeign.getPost(1L)
+) : BehaviorSpec({
+    Given("getPosts stub - success") {
+        val getPostsWireMockResponsePath = "payload/post.json"
+        FeignSuccessStub.getPosts(wireMockServer, getPostsWireMockResponsePath)
+
+        When("feign 으로 getPost 호출") {
+            val response = fakeApiOpenFeign.getPost(1L)
+
+            Then("response 일치") {
+                val wireMockResponsePath = "src/test/resources/__files/"
+                val expected: PostResponse = Gson().fromJson(
+                    JsonReader(FileReader(wireMockResponsePath + getPostsWireMockResponsePath)),
+                    PostResponse::class.java
+                )
+                response shouldBe expected
+            }
+        }
     }
 }) {
     override suspend fun beforeSpec(spec: Spec) {
--- src/test/kotlin/com/example/openfeignspringtutorial/testconfig/FeignSuccessStub.kt
+++ src/test/kotlin/com/example/openfeignspringtutorial/testconfig/FeignSuccessStub.kt
@@ -7,14 +7,14 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 
 object FeignSuccessStub {
-    fun getPosts(mockService: WireMockServer) {
+    fun getPosts(mockService: WireMockServer, wireMockResponsePath: String) {
         mockService.stubFor(
             WireMock.get(WireMock.urlPathMatching("/posts/([1-99])"))
                 .willReturn(
                     WireMock.aResponse()
                         .withStatus(HttpStatus.OK.value())
                         .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .withBodyFile("payload/post.json")
+                        .withBodyFile(wireMockResponsePath)
                 )
         )
     }
