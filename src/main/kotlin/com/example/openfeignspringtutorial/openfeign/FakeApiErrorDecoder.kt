168926cf1a076691f092564d948bd4c84e43b573
Hyune-c <choi8608@gmail.com>
2023-02-04 05:55:30 +0900

    ♻️ errorDecoder 설정을 yml 로 이동

--- src/main/kotlin/com/example/openfeignspringtutorial/openfeign/FakeApiErrorDecoder.kt
+++ src/main/kotlin/com/example/openfeignspringtutorial/openfeign/FakeApiErrorDecoder.kt
@@ -4,11 +4,9 @@ import feign.Response
 import feign.codec.ErrorDecoder
 import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
-import org.springframework.stereotype.Component
 import org.springframework.web.server.ResponseStatusException
 
-@Component
-class OpenFeignErrorDecoder : ErrorDecoder {
+class FakeApiErrorDecoder : ErrorDecoder {
     private val log = LoggerFactory.getLogger(this.javaClass)
 
     override fun decode(methodKey: String, response: Response): Exception {
