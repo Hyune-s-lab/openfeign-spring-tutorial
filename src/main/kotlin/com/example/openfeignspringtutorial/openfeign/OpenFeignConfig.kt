5f8f13e3e81e76d4a7a1f9a858358ae730fda984
Hyune-c <choi8608@gmail.com>
2023-02-04 05:16:51 +0900

    ✨ retry 구현 & 테스트 코드 리팩토링

--- src/main/kotlin/com/example/openfeignspringtutorial/openfeign/OpenFeignConfig.kt
+++ src/main/kotlin/com/example/openfeignspringtutorial/openfeign/OpenFeignConfig.kt
@@ -1,8 +1,15 @@
 package com.example.openfeignspringtutorial.openfeign
 
+import feign.Retryer
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableFeignClients("com.example.openfeignspringtutorial.openfeign")
-class OpenFeignConfig
+class OpenFeignConfig {
+    @Bean
+    fun retryer(): Retryer {
+        return Retryer.Default(300, 2000, 3)
+    }
+}
--- src/test/kotlin/com/example/openfeignspringtutorial/openfeign/FakeApiOpenFeignTest.kt
+++ src/test/kotlin/com/example/openfeignspringtutorial/openfeign/FakeApiOpenFeignTest.kt
@@ -4,12 +4,15 @@ import com.example.openfeignspringtutorial.openfeign.response.PostResponse
 import com.example.openfeignspringtutorial.testconfig.WireMockServerConfig
 import com.example.openfeignspringtutorial.testconfig.WireMockServerConst
 import com.github.tomakehurst.wiremock.WireMockServer
+import com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder
 import com.github.tomakehurst.wiremock.client.WireMock
 import com.google.gson.Gson
 import com.google.gson.stream.JsonReader
+import feign.RetryableException
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldStartWith
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
@@ -25,64 +28,72 @@ internal class FakeApiOpenFeignTest(
     private val fakeApiOpenFeign: FakeApiOpenFeign,
 ) : BehaviorSpec({
     Given("getPosts stub - success") {
-        wireMockServer.stubFor(
-            WireMock.get(WireMock.urlPathMatching(WireMockServerConst.GetPosts.url))
-                .willReturn(
-                    WireMock.aResponse()
-                        .withStatus(HttpStatus.OK.value())
-                        .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .withBodyFile(WireMockServerConst.GetPosts.responsePath)
-                )
+        wireMockServer.stubGetPosts(
+            WireMock.aResponse()
+                .withStatus(HttpStatus.OK.value())
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .withBodyFile(WireMockServerConst.GetPosts.responsePath)
         )
 
         When("feign 으로 getPost 호출") {
-            val response = fakeApiOpenFeign.getPost(1L)
+            val then = fakeApiOpenFeign.getPost(1L)
 
             Then("response 일치") {
                 val expected: PostResponse = Gson().fromJson(
                     JsonReader(FileReader(WireMockServerConst.GetPosts.absoluteResponsePath())),
                     PostResponse::class.java
                 )
-                response shouldBe expected
+                then shouldBe expected
             }
         }
     }
 
-    Given("getPosts stub - bad request") {
-        wireMockServer.stubFor(
-            WireMock.get(WireMock.urlPathMatching(WireMockServerConst.GetPosts.url))
-                .willReturn(
-                    WireMock.badRequest()
-                )
-        )
+    Given("getPosts stub - BAD_REQUEST") {
+        wireMockServer.stubGetPosts(WireMock.badRequest())
 
         When("feign 으로 getPost 호출") {
-            val exception = shouldThrow<ResponseStatusException> {
+            val then = shouldThrow<ResponseStatusException> {
                 fakeApiOpenFeign.getPost(1L)
             }
 
-            Then("bad request 확인") {
-                exception.status shouldBe HttpStatus.BAD_REQUEST
+            Then("BAD_REQUEST 확인") {
+                then.status shouldBe HttpStatus.BAD_REQUEST
             }
         }
     }
 
     Given("getPosts stub - INTERNAL_SERVER_ERROR") {
-        wireMockServer.stubFor(
-            WireMock.get(WireMock.urlPathMatching(WireMockServerConst.GetPosts.url))
-                .willReturn(
-                    WireMock.serverError()
-                )
-        )
+        wireMockServer.stubGetPosts(WireMock.serverError())
 
         When("feign 으로 getPost 호출") {
-            val exception = shouldThrow<ResponseStatusException> {
+            val then = shouldThrow<ResponseStatusException> {
                 fakeApiOpenFeign.getPost(1L)
             }
 
-            Then("status 확인") {
-                exception.status shouldBe HttpStatus.INTERNAL_SERVER_ERROR
+            Then("INTERNAL_SERVER_ERROR 확인") {
+                then.status shouldBe HttpStatus.INTERNAL_SERVER_ERROR
+            }
+        }
+    }
+
+    Given("getPosts stub - Connection refused") {
+        wireMockServer.stop()
+
+        When("feign 으로 getPost 호출") {
+            val then = shouldThrow<RetryableException> {
+                fakeApiOpenFeign.getPost(1L)
+            }
+
+            Then("Connection refused 확인") {
+                then.message shouldStartWith "Connection refused"
             }
         }
     }
 })
+
+private fun WireMockServer.stubGetPosts(responseDefinitionBuilder: ResponseDefinitionBuilder) {
+    stubFor(
+        WireMock.get(WireMock.urlPathMatching(WireMockServerConst.GetPosts.url))
+            .willReturn(responseDefinitionBuilder)
+    )
+}
